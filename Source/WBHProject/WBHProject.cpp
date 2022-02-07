1b5e4c38601f2f13fad81211bd0706ff7305ab66
RFreiwang <ruediger_freiwang@web.de>
2022-02-07 03:20:50 +0100

    VT Source

---
+++ Source/WBHProject/WBHProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WBHProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WBHProject, "WBHProject" );
---
+++ Source/WBHProject/WBHProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WBHProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WBHPROJECT_API AWBHProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
